334ec8fc859cccaf573e7a4628d6f438ab5f0ed9
Zaidan <znaufalilmi@gmail.com>
2025-05-23 11:20:53 +0700

    finish core ges

--- src-gen/Basic/run.bat
+++ src-gen/Basic/run.bat
@@ -1,4 +1,4 @@
-echo SELECT 'CREATE DATABASE restaurantservicesystem_product_basic' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'restaurantservicesystem_product_basic') \gexec | psql "postgresql://postgres:murifq@localhost"
-for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:murifq@localhost/restaurantservicesystem_product_basic"
+echo SELECT 'CREATE DATABASE restaurantservicesystem_product_basic' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'restaurantservicesystem_product_basic') \gexec | psql "postgresql://postgres:iniKUNCI123@localhost"
+for %%G in (sql/*.sql) do psql -a -f sql/%%G "postgresql://postgres:iniKUNCI123@localhost/restaurantservicesystem_product_basic"
 
 java -cp restaurantservicesystem.product.basic --module-path restaurantservicesystem.product.basic -m restaurantservicesystem.product.basic
--- src-gen/Basic/run.sh
+++ src-gen/Basic/run.sh
@@ -18,9 +18,9 @@ tail -f java.log --pid=$TEE_PID | while read -r LINE; do
     fi
 done
 
-echo "SELECT 'CREATE DATABASE restaurantservicesystem_product_basic' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'restaurantservicesystem_product_basic') \gexec" | psql "postgresql://postgres:murifq@localhost"
+echo "SELECT 'CREATE DATABASE restaurantservicesystem_product_basic' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'restaurantservicesystem_product_basic') \gexec" | psql "postgresql://postgres:iniKUNCI123@localhost"
 for file in sql/*.sql; do
-    psql -a -f "$file" "postgresql://postgres:murifq@localhost/restaurantservicesystem_product_basic"
+    psql -a -f "$file" "postgresql://postgres:iniKUNCI123@localhost/restaurantservicesystem_product_basic"
 done
 
 wait
